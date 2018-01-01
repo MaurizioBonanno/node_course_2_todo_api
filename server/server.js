097d19c900bfbef0f011da6006fa70beccedf944
maurizio <mauriziobonanno43@gmail.com>
2018-01-01 13:08:20 +0100

    mongoose

--- server/server.js
+++ server/server.js
@@ -1,28 +1,39 @@
-const mongoose = require('mongoose');
+var { mongoose } = require('./db/mongoose');
 
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-//Save something
+// //Save something
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
-});
 
-var newTodo = new Todo({
-    text: 'Cook dinner'
-});
-newTodo.save().then((todo) => {
-    console.log('Todo Salvato', todo);
-}, (e) => {
-    console.log('Non posso salvare i dati', e);
-});
+
+// // var newTodo = new Todo({
+// //     text: 'Cook dinner'
+// // });
+// // newTodo.save().then((todo) => {
+// //     console.log('Todo Salvato', todo);
+// // }, (e) => {
+// //     console.log('Non posso salvare i dati', e);
+// // });
+
+// // var otherTodo = new Todo({
+// //     text: 'Dar da mangiare al gatto',
+// //     completed: false,
+// //     completedAt: 123
+// // });
+
+// // otherTodo.save().then((doc) => {
+// //     console.log(JSON.stringify(doc, undefined, 2));
+// // }, (e) => {
+// //     console.log('Impossibile salvare i dati');
+// // });
+
+
+
+// var user = new User({
+//     email: 'io@example.com'
+// });
+// user.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log(e);
+// });
